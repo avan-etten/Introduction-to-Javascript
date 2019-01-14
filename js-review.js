03ad0f7598b69fca80387bbb250b690120f812c8
Alec <avan-etten@mail.greenriver.edu>
2019-01-14 15:34:01 -0500

    added countChar

--- js-review.js
+++ js-review.js
@@ -19,12 +19,27 @@ for (let i = 1; i <= 100; i++) {
 }
 
 function countBs(string){
+
     let count = 0;
     for (let i = 0; i < string.length; i++) {
 
-        if (string[i] === 'B'){
+        if (string[i] === 'B') {
             count++;
         }
     }
     return count;
+}
+
+function countChar(string, char){
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+
+        if (string[i] === char) {
+            count++;
+        }
+    }
+
+    return count;
+
+
 }
