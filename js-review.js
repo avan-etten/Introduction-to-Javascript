9b7f02e2225353f5b008c9dfa1cbeaa63f7e8117
Alec <avan-etten@mail.greenriver.edu>
2019-01-14 15:04:26 -0500

    added fizzbuzz

--- js-review.js
+++ js-review.js
@@ -1,1 +1,19 @@
 console.log("Introduction to Javascript");
+
+//FIZZBUZZ
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 == 0 || i % 5 == 0) {
+        if (i % 3 == 0) {
+            console.log("Fizz");
+        }
+        if (i % 5 == 0) {
+            console.log("Buzz");
+        }
+        if (i % 3 == 0 && i % 5 == 0){
+            console.log("FizzBuzz");
+        }
+    } else {
+        console.log(i);
+    }
+}
