26fb672bdcc98710c968bbad54e295e35f88ec09
Alec <avan-etten@mail.greenriver.edu>
2019-01-14 15:26:59 -0500

    added countBs

--- js-review.js
+++ js-review.js
@@ -3,17 +3,28 @@ console.log("Introduction to Javascript");
 //FIZZBUZZ
 for (let i = 1; i <= 100; i++) {
 
-    if (i % 3 == 0 || i % 5 == 0) {
-        if (i % 3 == 0) {
+    if (i % 3 === 0 || i % 5 === 0) {
+        if (i % 3 === 0) {
             console.log("Fizz");
         }
-        if (i % 5 == 0) {
+        if (i % 5 === 0) {
             console.log("Buzz");
         }
-        if (i % 3 == 0 && i % 5 == 0){
+        if (i % 3 === 0 && i % 5 === 0){
             console.log("FizzBuzz");
         }
     } else {
         console.log(i);
     }
+}
+
+function countBs(string){
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+
+        if (string[i] === 'B'){
+            count++;
+        }
+    }
+    return count;
 }
